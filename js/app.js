79c8c044d7a1180ca1b57079b17aa2328d9d5991
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-10-29 20:06:53 -0400

    added vet appointment

--- js/app.js
+++ js/app.js
@@ -13,6 +13,47 @@ const formulario = document.querySelector("#nueva-cita");
 //Contenedor donde irán las citas
 const contenedorCitas = document.querySelector("#citas");
 
+//Proyecto necesita 2 clases, para UI y otra para las citas
+//Clases
+class Citas {
+    constructor(){
+        this.citas = [];
+    }
+}
+
+class UI{
+    imprimirAlerta(mensaje, tipo){
+        //Crear el div
+        const divMensaje = document.createElement("div");
+        divMensaje.classList.add("text-center", "alert", "d-block", "col-12");
+
+        //Si es tipo error
+        if(tipo === "error"){
+            divMensaje.classList.add("alert-danger");
+        } else {
+            divMensaje.classList.add("alert-success");
+        }
+
+        //Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        //Agregar al DOM
+
+        document.querySelector("#contenido").insertBefore(divMensaje, document.querySelector(".agregar-cita"));
+
+        //Quitar la alerta después de unos segundos
+
+        setTimeout(()=>{
+            divMensaje.remove();
+        }, 2500);
+    }
+}
+
+//Instanciamos las clases de forma global
+const ui = new UI();
+const administrarCitas = new Citas();
+
+
 //eventListeners
 eventListeners();   
 function eventListeners() {
@@ -22,6 +63,8 @@ function eventListeners() {
     fechaInput.addEventListener("input", datosCita)
     horaInput.addEventListener("input", datosCita)
     sintomasInput.addEventListener("input", datosCita)
+
+    formulario.addEventListener("submit", nuevaCita);
 }
 
 const citaObj = { //Creamos un obj que se llene de acuerdo con el usuario llenando los campos
@@ -39,3 +82,14 @@ function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
 }
 
+//Valida y agrega una nueva cita a la clase de citas
+function nuevaCita(e) {
+    e.preventDefault();
+
+    //Extraemos los datos de citaObj
+    const {mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+    if(mascota === "" || propietario === "" || telefono === "" || fecha === "" || hora === "" || sintomas === "") {
+        ui.imprimirAlerta("Todos los campos son obligatorios", "error")
+        return; //El return para que no se ejecute la siguiente línea
+    }
+}
